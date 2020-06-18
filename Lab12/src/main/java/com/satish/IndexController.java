226493cd023816426fb83e1aac0c4e148c6e9118
Satish Prasad <62802567+spd61185@users.noreply.github.com>
2020-06-17 21:04:13 -0700

    Update IndexController.java

--- Lab12/src/main/java/com/satish/IndexController.java
+++ Lab12/src/main/java/com/satish/IndexController.java
@@ -5,11 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-	
 	@GetMapping("/")
 	public String showIndexPage() {
 		System.out.println("IndexController - showIndexPage()"); 
 		return "index";
 	}
-	
 }
